824221191a54c35e0dc2f4630a98d71a4feddc50
strushkov <strushkov@scrile.com>
2025-03-10 00:48:09 +0700

    create front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import HashCracker from "./components/HashCracker";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <h1 className="text-center font-bold">Hash Cracker</h1>
+          <HashCracker/>
+      </div>
   );
 }
 
